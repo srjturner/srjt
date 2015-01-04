5a429d7f0f0ba1784a4741a12efe2534d0c07472
Simon Turner <simon.turner@zen.co.uk>
2015-01-04 22:21:47 +0000

    Fixed broken SBT

--- project/build.scala
+++ project/build.scala
@@ -6,12 +6,13 @@ object SrjtBuild extends Build {
   lazy val commonSettings = Seq(
     organization := "com.example",
     version := "0.1.0",
-    scalaVersion := "2.10.3"
+    scalaVersion := "2.10.3",
+    scalacOptions ++= Seq("-feature", "-language:implicitConversions", "-language:reflectiveCalls")
   )
 
   val scalatest = "org.scalatest" %% "scalatest" % "1.9.1" % "test"
 
-  lazy val root = project.in(file(".").aggregate(scalalang)
+  lazy val root = project.in(file(".")).aggregate(scalalang)
 
   lazy val scalalang = project.settings(commonSettings: _*).settings(
     libraryDependencies += scalatest
--- scalalang/build.sbt
+++ scalalang/build.sbt
@@ -1,7 +1,3 @@
 name := "scalalang"
 
 version := "1.0"
-
-scalaVersion := "2.10.3"
-
-scalacOptions ++= Seq("-feature", "-language:implicitConversions", "-language:reflectiveCalls")
